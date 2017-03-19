4f2cd16637acf81e402c4a16421dd3123ee067b4
Marcus André <ddtank.marcus@gmail.com>
2017-03-19 20:59:08 -0300

    Initial Release

---
+++ ATGUI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using AutomataTranslator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ATGUI {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            openFileDialog1.ShowDialog();
+        }
+
+        private SMDManager Editor;
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            listBox1.Items.Clear();
+            Editor = new SMDManager(File.ReadAllBytes(openFileDialog1.FileName));
+            string[] strs = Editor.Import();
+            foreach (string str in strs)
+                listBox1.Items.Add(str);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) {
+            if (e.KeyChar == '\n' || e.KeyChar == '\r') {
+                try {
+                    listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+                }
+                catch { }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            try {
+                int i = listBox1.SelectedIndex;
+                Text = "ID: " + i + "/" + listBox1.Items.Count;
+                textBox1.Text = listBox1.Items[i].ToString();
+            }
+            catch { }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            string[] Strs = new string[listBox1.Items.Count];
+            for (int i = 0; i < Strs.Length; i++)
+                Strs[i] = listBox1.Items[i].ToString();
+            File.WriteAllBytes(saveFileDialog1.FileName, Editor.Export(Strs));
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+            saveFileDialog1.ShowDialog();
+        }
+    }
+}
---
+++ AutomataTranslator/Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomataTranslator
+{
+    public class SMDManager
+    {
+        byte[] Script;
+        long[] StrEntries;
+        public SMDManager(byte[] Script) {
+            this.Script = Script;
+        }
+
+        public string[] Import() {
+            uint StrCount = GetDW(Script, 0);
+            StrEntries = new long[StrCount];
+            string[] Strs = new string[StrCount];
+            long Pos = 4;
+            uint StrId = 0;
+            while (Pos < Script.Length) {
+                Pos += 0x88;
+                StrEntries[StrId] = Pos;
+                byte[] StrBuffer = new byte[0x800];
+                Copy(Script, Pos, ref StrBuffer, 0, 0x800);
+                Strs[StrId++] = Encoding.Unicode.GetString(StrBuffer).Replace("\x0", "");
+                Pos += 0x800;
+            }
+
+            return Strs;
+        }
+
+        public byte[] Export(string[] Strs) {
+            if (Strs.Length != StrEntries.Length)
+                throw new Exception("You can't add/remove string entries");
+            for (uint i = 0; i < Strs.Length; i++) {
+                byte[] Buffer = new byte[0x800];
+                byte[] Str = Encoding.Unicode.GetBytes(Strs[i]);
+                if (Str.Length > 0x800)
+                    throw new Exception("The biggest allowed length size is 0x800");
+                Str.CopyTo(Buffer, 0);
+                Copy(Buffer, 0, ref Script, StrEntries[i], 0x800);
+            }
+
+            return Script;
+        }
+        private void Copy(byte[] In, long ReadIndex, ref byte[] Out, long WriteIndex, long Length) {
+            for (long i = 0; i < Length; i++)
+                Out[i + WriteIndex] = In[ReadIndex + i];
+        }
+
+        private uint GetDW(byte[] File, long Pos) {
+            byte[] Arr = new byte[] { File[Pos + 0], File[Pos + 1], File[Pos + 2], File[Pos +3]};
+            return BitConverter.ToUInt32(Arr, 0);
+        }
+    }
+}
